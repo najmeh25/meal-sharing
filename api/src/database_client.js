a40f90a8a920b2de3c0341aec16192fdfde071d0
najmeh25 <seyedehnajmeh25@gmail.com>
2025-02-27 05:06:26 -0800

    add hw_meal_sharing

--- api/src/database_client.js
+++ api/src/database_client.js
@@ -1,4 +1,35 @@
 import knex from "knex";
+import "dotenv/config";
+
+
+const db = knex({
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "123456",
+    database: process.env.DB_NAME || "meal_sharing",
+  },
+  pool: { min: 2, max: 10 } 
+});
+
+
+db.raw("SELECT 1")
+  .then(() => console.log("✅ Connected to MySQL database successfully!"))
+  .catch((err) => {
+    console.error("❌ Failed to connect to MySQL:", err);
+    process.exit(1);
+  });
+
+export default db;
+
+
+
+
+
+
+
+/*import knex from "knex";
 
 const connection = knex({
   client: process.env.DB_CLIENT,
@@ -13,4 +44,4 @@ const connection = knex({
   },
 });
 
-export default connection;
+export default connection;*/
